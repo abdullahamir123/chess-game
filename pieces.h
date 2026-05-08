87ef1496573b733cb5254d23342b5c61f9875ccc
Rayan <rayanather80@gmail.com>
2026-05-08 23:33:44 +0500

    made check.cpp and check.h also fixed pawn for diagonal and also in render.h -> for pawn

---
+++ pieces.h
@@ -0,0 +1,79 @@
+#pragma once
+
+enum class color { white, black };
+
+struct Cord {
+    int row;
+    int col;
+};
+
+
+class Piece {
+protected:
+    color pieceColor;
+    Cord position;
+
+public:
+    Piece(color c, int row, int col);
+    virtual ~Piece() = default;
+
+    color getColor() const;
+    Cord getPosition() const;
+
+    void setPosition(int row, int col);
+    bool noFriendlyCapture(int row, int col) const;
+    bool isPathClear(int row, int col) const;
+
+    virtual int getTypeId() const = 0;
+    virtual bool validmove(int row, int col)  = 0;
+};
+
+
+class Pawn : public Piece {
+public:
+    bool hasMoved;
+    Pawn(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col)  override;
+    bool isPawnPathClear(int row);
+    bool pawn_attack(int row, int col) const;
+};
+
+class Rook : public Piece {
+public:
+    Rook(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col) override;
+};
+
+class Knight : public Piece {
+public:
+    Knight(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col) override;
+
+};
+
+class Bishop : public Piece {
+public:
+    Bishop(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col) override;
+
+};
+
+class Queen : public Piece {
+public:
+    Queen(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col) override;
+
+};
+
+class King : public Piece {
+public:
+    King(color c, int row, int col);
+    int getTypeId() const override;
+    bool validmove(int row, int col) override;
+
+};
---
+++ render.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "raylib.h"
+#include "pieces.h"
+
+class Render {
+public:
+    static Piece* board[8][8];
+    static Texture2D textures[12];
+
+    static void LoadTextures();
+
+    static void initBoard();
+    static void window();
+
+    static void mainGrid();
+    static void leftGrid();
+    static void rightGrid();
+
+    static void mouse();
+
+    static int selectedRow;
+    static int selectedCol;
+    static Piece* selectedPiece;
+    static bool whiteTurn;
+};
