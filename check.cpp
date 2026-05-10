2321b9d7b8dd3c189b29f9a16c51f6dff0374b7d
Abdullah Amir <123abdullahamir@gmail.com>
2026-05-10 13:58:52 +0500

    resolved all bugs that came with conflict

--- check.cpp
+++ check.cpp
@@ -1,7 +1,6 @@
 #include "check.h"
 #include "render.h"
-// this function always keep track where the king is present so we can know if at that positio
-// can any other enemy piece can move
+
 Cord Check::locate_king(color kingColor) {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
@@ -24,7 +23,6 @@ Cord Check::locate_king(color kingColor) {
     return not_found;
 }
 
-// this fucntion now cheking if king being check or not
 bool Check::king_check(color color_king) {
     Cord location_king = locate_king(color_king);
     for (int i = 0; i < 8; i++) {
@@ -37,7 +35,6 @@ bool Check::king_check(color color_king) {
                 continue;
             }
 
-            // this only for pawn as if it can attack dignoally so i am cheking this way
             if (enemy->canAttack(location_king.row, location_king.col)) {
                 return true;
             }
@@ -50,20 +47,13 @@ bool Check::piece_move_checkmate(color kingColor) {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             Piece* piece = Render::board[i][j];
-            if (piece == nullptr) {
-                continue;
-            }
-            if (piece->getColor() != kingColor) {
-                continue;
-            }
+            if (piece == nullptr) continue;
+            if (piece->getColor() != kingColor) continue;
+
             for (int row = 0; row < 8; row++) {
                 for (int col = 0; col < 8; col++) {
-                    if (!piece->validmove(row, col)) {
-                        continue;
-                    }
-                    if (!piece->noFriendlyCapture(row, col)) {
-                        continue;
-                    }
+                    if (!piece->validmove(row, col)) continue;
+                    if (!piece->noFriendlyCapture(row, col)) continue;
                     if (after_move_no_checkmate(piece, row, col)) {
                         return true;
                     }
@@ -76,38 +66,48 @@ bool Check::piece_move_checkmate(color kingColor) {
 
 bool Check::after_move_no_checkmate(Piece* piece, int targetRow, int targetCol) {
     Cord position_king = piece->getPosition();
-
     Piece* capture_piece = Render::board[targetRow][targetCol];
+
+    bool isEnPassant = false;
+    Piece* enPassantPawn = nullptr;
+    int epRow = -1, epCol = -1;
+
+    if (piece->getTypeId() == 5 && capture_piece == nullptr && targetCol != position_king.col) {
+        isEnPassant = true;
+        epRow = position_king.row;
+        epCol = targetCol;
+        enPassantPawn = Render::board[epRow][epCol];
+        Render::board[epRow][epCol] = nullptr;
+    }
+
     Render::board[position_king.row][position_king.col] = nullptr;
     Render::board[targetRow][targetCol] = piece;
 
     piece->setPosition(targetRow, targetCol);
     bool safe = !king_check(piece->getColor());
+
     Render::board[position_king.row][position_king.col] = piece;
     Render::board[targetRow][targetCol] = capture_piece;
     piece->setPosition(position_king.row, position_king.col);
+
+    if (isEnPassant) {
+        Render::board[epRow][epCol] = enPassantPawn;
+    }
+
     return safe;
 }
 
-
-
-
 bool Check::checking_stalemate(color kingColor) {
-    //if king in check, not stalemate
     if (king_check(kingColor))
         return false;
 
-    //check if valid move, return true if so
     if (piece_move_checkmate(kingColor))
         return false;
 
-    //if no moves or no check then stalemate
     return true;
-
 }
 
 bool Check::checking_checkmate(color kingColor) {
-
     if (!king_check(kingColor)) {
         return false;
     }
@@ -116,4 +116,3 @@ bool Check::checking_checkmate(color kingColor) {
     }
     return true;
 }
-
--- render.cpp
+++ render.cpp
@@ -1,12 +1,12 @@
 #include "render.h"
 #include "check.h"
 #include "pieces.h"
-#include<iostream>
+#include <iostream>
+#include <cmath>
 
 const int width = 1600;
 const int height = 1100;
 const float cellSize = (width - 500.0f) / 8.0f;
-
 
 int Render::selectedRow = -1;
 int Render::selectedCol = -1;
@@ -30,8 +30,7 @@ bool Render::white_checkmate = false;
 bool Render::black_checkmate = false;
 bool Render::white_stalemate = false;
 bool Render::black_stalemate = false;
-//loads all assets to memory
-//for white variant, 0 to 5. for the black variant of same type, +6
+
 void Render::LoadTextures() {
     textures[0] = LoadTexture("assets/white-rook.png");
     textures[1] = LoadTexture("assets/white-knight.png");
@@ -49,8 +48,6 @@ void Render::LoadTextures() {
     endGameTexture = LoadTexture("assets/cat endpopup.png");
 }
 
-
-//initialize specific indices of the board[][] as piece type pointers. remaining will stay nullptr
 void Render::initBoard() {
     for (int i = 0; i < 8; i++) {
         board[1][i] = new Pawn(color::black, 1, i);
@@ -78,7 +75,7 @@ void Render::initBoard() {
 
 void Render::window() {
     InitWindow(width, height, "chess game");
-    font = LoadFontEx("assets/FreeSans.otf", 32, NULL, 0);
+    font = LoadFontEx("assets/FreeSans.otf", 42, NULL, 0);
 
     LoadTextures();
     initBoard();
@@ -89,7 +86,6 @@ void Render::window() {
         }
 
         BeginDrawing();
-
         mainGrid();
         leftGrid();
         rightGrid();
@@ -100,7 +96,6 @@ void Render::window() {
         drawPopup();
         EndDrawing();
     }
-
     CloseWindow();
 }
 
@@ -121,7 +116,6 @@ void Render::mainGridMouse() {
         return;
     }
 
-    //if selectedpiece remains same. we unselect
     if (selectedPiece && selectedRow == row && selectedCol == col) {
         selectedPiece = nullptr;
         selectedRow = -1;
@@ -129,7 +123,6 @@ void Render::mainGridMouse() {
         return;
     }
 
-    //if selected piece color same as next selection, change selected piece to new (selecting a diff piece to move)
     if (board[row][col] && ((board[row][col]->getColor() == color::white && whiteTurn) || (board[row][col]->getColor() == color::black && !whiteTurn))) {
         selectedPiece = board[row][col];
         selectedRow = row;
@@ -145,19 +138,23 @@ void Render::mainGridMouse() {
         if (move_making_king_check(selectedPiece, row, col)) {
             return;
         }
-        if (board[row][col] != nullptr) {
-            int capturedId = board[row][col]->getTypeId();
-            color capturedColor = board[row][col]->getColor();
 
-        //special moves execution
+        //execute special moves independednt of standard capturing
 
-        //handle en passant capture (pawn captured empty square diagonally)
+        // Handle En Passant capture
         if (selectedPiece->getTypeId() == 5 && col == enPassantCol && row == enPassantRow + (selectedPiece->getColor() == color::white ? -1 : 1)) {
-            delete board[enPassantRow][enPassantCol];
-            board[enPassantRow][enPassantCol] = nullptr;
+            if (board[enPassantRow][enPassantCol] != nullptr) {
+                int capturedId = board[enPassantRow][enPassantCol]->getTypeId();
+                color capturedColor = board[enPassantRow][enPassantCol]->getColor();
+                if (capturedColor == color::white) whiteCaptured.push_back(capturedId);
+                else blackCaptured.push_back(capturedId);
+
+                delete board[enPassantRow][enPassantCol];
+                board[enPassantRow][enPassantCol] = nullptr;
+            }
         }
 
-        //handle castling (move the rook as well)
+        //handle castling
         if (selectedPiece->getTypeId() == 4 && std::abs(col - selectedCol) == 2) {
             int rookOldCol = (col > selectedCol) ? 7 : 0;
             int rookNewCol = (col > selectedCol) ? 5 : 3;
@@ -170,119 +167,116 @@ void Render::mainGridMouse() {
             }
         }
 
-        //en passant availability for next turn
+        //en passant availability window creation
         int currentSelectedRow = selectedRow;
         if (selectedPiece->getTypeId() == 5 && std::abs(row - currentSelectedRow) == 2) {
             enPassantCol = col;
             enPassantRow = row;
         }
         else {
-            enPassantCol = -1; // Reset if any other move is made
+            enPassantCol = -1;
         }
 
-            // basically adding the captured id into the white list for the piece captured
+        //handle standard capture logic
+        if (board[row][col] != nullptr) {
+            int capturedId = board[row][col]->getTypeId();
+            color capturedColor = board[row][col]->getColor();
+
             if (capturedColor == color::white) {
                 whiteCaptured.push_back(capturedId);
             }
-            // vice versa
             else {
                 blackCaptured.push_back(capturedId);
             }
-            //delete
             delete board[row][col];
         }
+
         board[selectedRow][selectedCol] = nullptr;
         board[row][col] = selectedPiece;
         selectedPiece->setPosition(row, col);
         selectedPiece->afterMove();
 
-
         selectedPiece = nullptr;
         selectedRow = -1;
         selectedCol = -1;
 
-        // turn switch
         whiteTurn = !whiteTurn;
-        //checks if checkmate, stalemate or check
         updateState();
     }
 }
 
-// check.cpp linking it to this render.cpp
 bool Render::move_making_king_check(Piece* piece, int row, int col) {
-
     Cord king_position = piece->getPosition();
     Piece* old_piece = Render::board[row][col];
 
-    // temporary move
+    bool isEnPassant = false;
+    Piece* enPassantPawn = nullptr;
+    int epRow = -1, epCol = -1;
+
+    //seperately for en passant
+    if (piece->getTypeId() == 5 && old_piece == nullptr && col != king_position.col) {
+        isEnPassant = true;
+        epRow = king_position.row;
+        epCol = col;
+        enPassantPawn = Render::board[epRow][epCol];
+        Render::board[epRow][epCol] = nullptr;
+    }
+
     Render::board[king_position.row][king_position.col] = nullptr;
     Render::board[row][col] = piece;
     piece->setPosition(row, col);
 
-    // it is checking if king becomes checked or not
     bool check = Check::king_check(piece->getColor());
 
-    // now we undoing the move we did
     Render::board[king_position.row][king_position.col] = piece;
     Render::board[row][col] = old_piece;
     piece->setPosition(king_position.row, king_position.col);
+
+    if (isEnPassant) {
+        Render::board[epRow][epCol] = enPassantPawn;
+    }
+
     return check;
 }
 
-
-//creates a main grid where each rectangle is seperately drawn
 void Render::mainGrid() {
     float offsetX = 250.0f;
-
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-
             float x = offsetX + j * cellSize;
             float y = i * cellSize;
 
             Rectangle cell = { x, y, cellSize, cellSize };
-
             Color col = ((i + j) % 2) ? BROWN : DARKBROWN;
 
-            //selected rectangle becomes yellow
             if (i == selectedRow && j == selectedCol) {
                 col = YELLOW;
             }
-            // this for checkmate and check
             if (board[i][j] && board[i][j]->getTypeId() == 4) {
-                if (board[i][j]->getColor() == color::white && white_check) {
-                    col = RED;
-                }
-                if (board[i][j]->getColor() == color::black && black_check) {
-                    col = RED;
-                }
+                if (board[i][j]->getColor() == color::white && white_check) col = RED;
+                if (board[i][j]->getColor() == color::black && black_check) col = RED;
             }
             DrawRectangleRec(cell, col);
 
-            //draws the pieces
             Piece* p = board[i][j];
             if (p) {
                 int base = (p->getColor() == color::white) ? 0 : 6;
                 int texIndex = base + p->getTypeId();
-
                 DrawTexture(textures[texIndex], x, y, WHITE);
             }
         }
     }
 }
 
-
-void Render::leftGrid(){
+void Render::leftGrid() {
     DrawRectangle(0, 0, 250, height, LIGHTGRAY);
 }
 
-void Render::rightGrid(){
+void Render::rightGrid() {
     DrawRectangle(width - 250, 0, 250, height, LIGHTGRAY);
 }
 
-
 void Render::promotionGrid(Piece* selectedPiece) {
-
     float offsetX = 400;
     float offsetY = 500;
     float popupWidth = 800;
@@ -290,10 +284,7 @@ void Render::promotionGrid(Piece* selectedPiece) {
     float popupCell = popupWidth / 4;
     Vector2 pos = { offsetX + 200, offsetY };
 
-    Rectangle popup = {
-        offsetX, offsetY,
-        popupWidth, popupHeight
-    };
+    Rectangle popup = { offsetX, offsetY, popupWidth, popupHeight };
     DrawRectangleRounded(popup, 0.5, 10, LIGHTGRAY);
     DrawTextEx(font, "Choose a piece to promote to", pos, 32, 2, BLACK);
     int textureIndex = (selectedPiece->getColor() == color::white) ? 0 : 6;
@@ -302,7 +293,6 @@ void Render::promotionGrid(Piece* selectedPiece) {
     }
     promotionMouse(offsetX, offsetY, popupCell);
 }
-
 
 void Render::promotionMouse(float offsetX, float OffsetY, float popupCell) {
     Vector2 mouse = GetMousePosition();
@@ -320,48 +310,34 @@ void Render::promotionMouse(float offsetX, float OffsetY, float popupCell) {
     delete board[promotionCord.row][promotionCord.col];
 
     switch (choice) {
-    case 0:
-        board[promotionCord.row][promotionCord.col] = new Rook(promotionColor, promotionCord.row, promotionCord.col);
-        break;
-    case 1:
-        board[promotionCord.row][promotionCord.col] = new Knight(promotionColor, promotionCord.row, promotionCord.col);
-        break;
-    case 2:
-        board[promotionCord.row][promotionCord.col] = new Bishop(promotionColor, promotionCord.row, promotionCord.col);
-        break;
-    case 3:
-        board[promotionCord.row][promotionCord.col] = new Queen(promotionColor, promotionCord.row, promotionCord.col);
-        break;
+    case 0: board[promotionCord.row][promotionCord.col] = new Rook(promotionColor, promotionCord.row, promotionCord.col); break;
+    case 1: board[promotionCord.row][promotionCord.col] = new Knight(promotionColor, promotionCord.row, promotionCord.col); break;
+    case 2: board[promotionCord.row][promotionCord.col] = new Bishop(promotionColor, promotionCord.row, promotionCord.col); break;
+    case 3: board[promotionCord.row][promotionCord.col] = new Queen(promotionColor, promotionCord.row, promotionCord.col); break;
     }
 
     promotionPiece = nullptr;
     updateState();
 }
 
-//basically after every move this function is called and it checks for check, stalemate and checkmate
 void Render::updateState() {
-    // checking if checkmate or stalemate against white
     white_checkmate = Check::checking_checkmate(color::white);
     white_stalemate = Check::checking_stalemate(color::white);
 
-    // vice versa
     black_checkmate = Check::checking_checkmate(color::black);
     black_stalemate = Check::checking_stalemate(color::black);
 
-    // checking for check
     white_check = Check::king_check(color::white);
     black_check = Check::king_check(color::black);
 }
 
 void Render::drawPopup() {
     float W = 640, H = 280;
-    //checking if ture for pop up
     bool isStalemate = (white_stalemate || black_stalemate);
     if (!white_checkmate && !black_checkmate && !isStalemate) {
         return;
     }
 
-    // blur background
     DrawRectangle(0, 0, 1600, 1100, Fade(BLACK, 0.75f));
 
     float x = (1600 - W) / 2.0f;
@@ -373,15 +349,12 @@ void Render::drawPopup() {
     Color highlight = BLACK;
     if (white_checkmate) {
         subtext = "Black Wins";
-        highlight = BLACK;
     }
     else if (black_checkmate) {
         subtext = "White Wins";
-        highlight = BLACK;
     }
     else if (isStalemate) {
         subtext = "Stalemate";
-        highlight = BLACK;
     }
     Vector2 headSize = MeasureTextEx(font, "Match Ended", 42, 2);
     Vector2 subSize = MeasureTextEx(font, subtext, 28, 1);
@@ -393,22 +366,19 @@ void Render::drawPopup() {
 }
 
 void Render::drawCapture() {
-    float scale = 0.4f; 
-    //increasing space between the pieces
+    float scale = 0.4f;
     float spacingX = 38.0f;
     float spacingY = 46.0f;
-  //display pieces captured by white on bottom right and for black on top left
     for (int i = 0; i < whiteCaptured.size(); i++) {
-        int texIndex = whiteCaptured[i]; 
+        int texIndex = whiteCaptured[i];
         float x = 10 + (i % 6) * spacingX;
         float y = 10 + (i / 6) * spacingY;
         DrawTextureEx(textures[texIndex], { x, y }, 0, scale, WHITE);
     }
-
     for (int i = 0; i < blackCaptured.size(); i++) {
-        int texIndex = blackCaptured[i] + 6; 
+        int texIndex = blackCaptured[i] + 6;
         float x = 1360 + (i % 6) * spacingX;
-        float y = 1050 - (i / 6) * spacingY; 
+        float y = 1050 - (i / 6) * spacingY;
         DrawTextureEx(textures[texIndex], { x, y }, 0, scale, WHITE);
     }
 }
